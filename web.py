0c0779f1c5304546c09ee66616d82a62a748a734
sudharsan-ai <sutharsan20202021@gmail.com>
2024-10-26 11:19:18 +0530

    success

--- web.py
+++ web.py
@@ -1,27 +1,3 @@
-# EX01 Developing a Simple Webserver
-## Date:
-
-## AIM:
-To develop a simple webserver to serve html pages and display the configuration details of laptop.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
-'''
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 content="""
@@ -92,15 +68,3 @@ print("Your web server is running....")
 server_address =('',8000)
 httpd = HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-'''
-
-
-## OUTPUT:
-![alt text](<Screenshot 2024-10-24 162611.png>)
-![alt text](<Screenshot 2024-10-24 163905.png>)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
-
-
-[def]: <Screenshot 2024-10-24 162611.png>
